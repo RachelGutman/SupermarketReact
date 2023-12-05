0a57e4b513eb53c31b0d879cce3a7506decb3572
RachelGutman <rachel5187@gmail.com>
2023-12-05 17:19:48 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/SupermarketReact/',
+  base: '/SupermarketReact/',
   plugins: [react()],
 })
